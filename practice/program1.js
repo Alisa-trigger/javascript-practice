3018ab384d2c3a5374d41089e8e534504d293eae
Alisa-trigger <firstyearclassics11@gmail.com>
2026-07-18 15:28:05 +0500

    Counter Program

---
+++ practice/program1.js
@@ -0,0 +1,24 @@
+// counter program
+
+
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const increaseBtn = document.getElementById("increase");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
