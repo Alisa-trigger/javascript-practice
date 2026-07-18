989c077e124d660a0fec51e7a3f5828137a69a27
Alisa-trigger <firstyearclassics11@gmail.com>
2026-07-18 15:03:48 +0500

    type conversion

---
+++ practice/typeconversion.js
@@ -0,0 +1,24 @@
+//  let age = window.prompt("What is your age?");
+
+//  age = Number(age);
+//  age++;
+
+// console.log(age, typeof age); 
+
+
+let x;
+let y;
+let z;
+
+
+x = "pizza";
+y= "pizza";
+z= "pizza";
+
+x = Number(x);
+y = Boolean(y);
+z= String(z); 
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
